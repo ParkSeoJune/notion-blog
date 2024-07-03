8b08624526962eb514de291749aeace019593a7c
ParkSeoJune <tjwns4122@gmail.com>
2024-07-03 11:51:36 +0900

    style: header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -9,35 +9,33 @@ import {
 import Link from "next/link";
 
 const Header = () => (
-  <header>
-    <Navbar shouldHideOnScroll isBordered>
-      <NavbarBrand className="w-fit">
-        <p className="font-bold">Notion Blog</p>
-      </NavbarBrand>
-      <NavbarContent as="div" className="items-center" justify="center">
-        <Input
-          classNames={{
-            base: "max-w-full sm:min-w-[20rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper:
-              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Link href="/" color="foreground">
-            Posts
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  </header>
+  <Navbar shouldHideOnScroll isBordered maxWidth="full" className="px-4">
+    <NavbarBrand>
+      <p className="font-bold">Notion Blog</p>
+    </NavbarBrand>
+    <NavbarContent as="div" className="items-center" justify="center">
+      <Input
+        classNames={{
+          base: "max-w-full sm:min-w-[20rem] h-10",
+          mainWrapper: "h-full",
+          input: "text-small",
+          inputWrapper:
+            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+        }}
+        placeholder="Type to search..."
+        size="sm"
+        startContent={<SearchIcon size={18} />}
+        type="search"
+      />
+    </NavbarContent>
+    <NavbarContent justify="end">
+      <NavbarItem>
+        <Link href="/" color="foreground">
+          Posts
+        </Link>
+      </NavbarItem>
+    </NavbarContent>
+  </Navbar>
 );
 
 export default Header;
