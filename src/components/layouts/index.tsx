c23de5a1c3d86b9790b44bd00f595df602f705a7
ParkSeoJune <tjwns4122@gmail.com>
2024-07-04 10:36:09 +0900

    style: footer position

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -4,11 +4,13 @@ import Footer from "./footer";
 import Header from "./header";
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <>
+  <div className="flex flex-col min-h-screen">
     <Header />
-    <main className="dark text-foreground bg-background">{children}</main>
+    <main className="flex-grow dark text-foreground bg-background">
+      {children}
+    </main>
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
