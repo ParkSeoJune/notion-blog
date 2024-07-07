90cc7a7de5ee5a93fade3e83862b0b5266efa1e0
ParkSeoJune <tjwns4122@gmail.com>
2024-07-08 08:31:53 +0900

    feat: hide api

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,17 +2,18 @@ import Layout from "@/components/layouts";
 import RecentPosts from "@/components/pages/recent-posts";
 import { fetchBlogData } from "@/services/api/main";
 
-async function fetchRecentPostsData() {
-  const data = await fetchBlogData({ count: 8 });
-  return data;
-}
+// async function fetchRecentPostsData() {
+//   const data = await fetchBlogData({ count: 8 });
+//   return data;
+// }
 
 export default async function Home() {
-  const data = await fetchRecentPostsData();
+  // const data = await fetchRecentPostsData();
 
   return (
     <Layout>
-      <RecentPosts initialData={data} />
+      {/* <RecentPosts initialData={data} /> */}
+      <div>h</div>
     </Layout>
   );
 }
