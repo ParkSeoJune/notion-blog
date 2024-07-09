ef501ef89f62d7b4909954aafe9301475e1784f8
ParkSeoJune <tjwns4122@gmail.com>
2024-07-10 08:22:06 +0900

    feat: post detail page metadata

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import { type Metadata } from "next";
 import { getPageTitle } from "notion-utils";
 
 import Layout from "@/components/layouts";
@@ -9,6 +10,23 @@ type PostDetailPageProps = {
     postId: string;
   };
 };
+
+export async function generateMetadata({
+  params,
+}: PostDetailPageProps): Promise<Metadata> {
+  const pageId = params.postId;
+  const recordMap = await getNotionPage(pageId);
+  const title = getPageTitle(recordMap);
+  return {
+    title: `${title}`,
+    alternates: {
+      canonical: `https://notion-blog-parkseojunes-projects.vercel.app/post${pageId}`,
+    },
+    openGraph: {
+      title: `${title}`,
+    },
+  };
+}
 
 const PostDetailPage = async ({ params }: PostDetailPageProps) => {
   const recordMap = await getNotionPage(params.postId);
