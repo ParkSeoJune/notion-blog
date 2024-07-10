bf10c39bb657ec25f4ef92a4f4a8d1654d2bf3a9
ParkSeoJune <tjwns4122@gmail.com>
2024-07-10 10:34:34 +0900

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { fetchBlogData } from "@/services/api/main";
+import type { Blog } from "@/types/posts";
+
+export default async function sitemap() {
+  const baseUrl = "https://notion-blog-parkseojunes-projects.vercel.app";
+  const posts = await fetchBlogData();
+  const postSitemaps = posts.map((post: Blog) => {
+    return {
+      url: `${baseUrl}/post/${post.id}`,
+      lastModified: new Date(),
+      priority: 0.7,
+      changeFrequency: "daily",
+    };
+  });
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      priority: 0.7,
+      changeFrequency: "daily",
+    },
+    {
+      url: `${baseUrl}/post`,
+      lastModified: new Date(),
+      priority: 0.7,
+      changeFrequency: "daily",
+    },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+      priority: 0.7,
+      changeFrequency: "daily",
+    },
+    ...postSitemaps,
+  ];
+}
