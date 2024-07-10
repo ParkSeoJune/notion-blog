19fdf56ed73488ebe66870e626247ef92be85457
ParkSeoJune <tjwns4122@gmail.com>
2024-07-10 11:31:46 +0900

    feat: change script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,15 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7110063599433500"
           crossOrigin="anonymous"
         ></script>
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block", textAlign: "center" }}
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-7110063599433500"
+          data-ad-slot="7435531374"
+        ></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta
           name="google-site-verification"
