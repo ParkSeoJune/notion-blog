b85520d74204ae1f0118ccb9e2745dee50e8614f
ParkSeoJune <tjwns4122@gmail.com>
2024-07-18 16:18:03 +0900

    feat: update visitor count api in layout

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -3,14 +3,27 @@ import { type ReactNode } from "react";
 import Footer from "./footer";
 import Header from "./header";
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <div className="flex flex-col min-h-screen">
-    <Header />
-    <main className="flex-grow flex justify-center dark text-foreground bg-background">
-      {children}
-    </main>
-    <Footer />
-  </div>
-);
+async function updateVisitorCount() {
+  const res = await fetch("/api/update-visitor-count");
+  if (res.ok) {
+    await res.json();
+  } else {
+    console.error("Error updating visitor count:", res.statusText);
+  }
+}
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  await updateVisitorCount();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow flex justify-center dark text-foreground bg-background">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
--- src/components/visitor-count.tsx
+++ src/components/visitor-count.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { db } from "@/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { format } from "date-fns";
@@ -8,24 +8,6 @@ import { format } from "date-fns";
 const VisitorCount = () => {
   const [todayCount, setTodayCount] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
-  const hasUpdatedVisitorCount = useRef(false);
-
-  useEffect(() => {
-    if (hasUpdatedVisitorCount.current) return;
-
-    const updateVisitorCount = async () => {
-      const res = await fetch("/api/update-visitor-count");
-      if (res.ok) {
-        await res.json();
-
-        hasUpdatedVisitorCount.current = true;
-      } else {
-        console.error("Error updating visitor count:", res.statusText);
-      }
-    };
-
-    updateVisitorCount();
-  }, []);
 
   useEffect(() => {
     const fetchVisitorData = async () => {
